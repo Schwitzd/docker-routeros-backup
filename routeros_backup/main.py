f757658e2d1cbbccff0ced7dd5db4636bbee089c
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-04-21 12:32:29 +0200

    refactor(main): wrap logic in main function and delegate error handling to internal components

--- routeros_backup/main.py
+++ routeros_backup/main.py
@@ -1,8 +1,16 @@
-from pathlib import Path
-from routeros_backup.destinations.s3 import S3Uploader
 from routeros_backup.config import Settings
+from routeros_backup.backup.ssh import perform_backup
+from routeros_backup.destinations.s3 import S3Uploader
 
-settings = Settings()
-uploader = S3Uploader(settings)
-uploader.connect()
-uploader.upload(Path("/tmp/routeros-2025-04-21.backup"))
+
+def main():
+    settings = Settings()
+    backup_path = perform_backup()
+
+    uploader = S3Uploader(settings)
+    uploader.connect()
+    uploader.upload(backup_path)
+
+
+if __name__ == "__main__":
+    main()
