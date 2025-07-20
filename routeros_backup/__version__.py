45c82a69c15fb16c24e65fa51d93d9ffa43fc3a9
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-07-20 11:15:56 +0200

    chore(build): switch to python-semantic-release for automated versioning and releases

---
+++ routeros_backup/__version__.py
@@ -0,0 +1,4 @@
+__title__ = "RouterOS Backup"
+__version__ = "0.5.2"
+__description__ = "A Docker-based tool to automate MikroTik RouterOS binary backup over SSH and support for uploading to external storage."
+__author__ = "Daniel Schwitzgebel"
