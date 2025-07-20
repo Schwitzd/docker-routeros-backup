80d7cf77a1fc73cee7f8ac56fb6e16d3899f4666
semantic-release <semantic-release>
2025-07-20 09:24:52 +0000

    1.5.0
    Automatically generated by python-semantic-release

--- routeros_backup/__about__.py
+++ routeros_backup/__about__.py
@@ -1,4 +1,4 @@
 __title__ = "RouterOS Backup"
-__version__ = "0.5.2"
+__version__ = "1.5.0"
 __description__ = "A Docker-based tool to automate MikroTik RouterOS binary backup over SSH and support for uploading to external storage."
 __author__ = "Daniel Schwitzgebel"
